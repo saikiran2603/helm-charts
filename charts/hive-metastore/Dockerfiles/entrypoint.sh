b9a079801432b26906bf8a1f36b481dd32713c81
Sai Kiran <neo2603@gmail.com>
2022-08-23 16:17:28 +0530

    Update

--- charts/hive-metastore/Dockerfiles/entrypoint.sh
+++ charts/hive-metastore/Dockerfiles/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-${HIVE_HOME}/bin/schematool -dbType mysql -validate
-if [[ $? -ne 0 ]];
-then
-  ${HIVE_HOME}/bin/schematool -dbType mysql -initSchema
-fi;
+${HIVE_HOME}/bin/schematool -dbType mysql -validate || ${HIVE_HOME}/bin/schematool -dbType mysql -initSchema
 
 # ${HIVE_HOME}/bin/schematool -initSchema -dbType mysql
 ${HIVE_HOME}/bin/start-metastore
