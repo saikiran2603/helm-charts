53a4844b112ec61e15d0ffdb0d42d5324be56b6f
Sai Kiran <neo2603@gmail.com>
2022-08-23 14:23:33 +0530

    Validate before init schema

--- charts/hive-metastore/Dockerfiles/entrypoint.sh
+++ charts/hive-metastore/Dockerfiles/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-${HIVE_HOME}/bin/schematool -initSchema -ifNotExists -dbType mysql
+${HIVE_HOME}/bin/schematool -dbType mysql -validate
+if [[ $? -ne 0 ]];
+then
+  ${HIVE_HOME}/bin/schematool -dbType mysql -initSchema
+fi;
+
+# ${HIVE_HOME}/bin/schematool -initSchema -dbType mysql
 ${HIVE_HOME}/bin/start-metastore
